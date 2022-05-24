b4d8fb45f9a8506b73f7804d54631b92dc333a34
marilia-borgo <marilia.bm01@gmail.com>
2022-05-24 10:27:59 -0300

    exercicio 1 feito

--- secao_01_estrutura_sequencial/ex_01_alo_mundo.py
+++ secao_01_estrutura_sequencial/ex_01_alo_mundo.py
@@ -12,4 +12,10 @@ Escreva um programa que mostre a mensagem "Olá Mundo"
 
 def ola_mundo():
     print ("Olá Mundo")
+ola_mundo()
+
+
+    
+
+
 
--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -12,5 +12,4 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 """
 
 def escreva_um_numero():
-    """Escreva aqui em baixo a sua solução"""
-
+   print('helloclear')
