3a561dbcbb5eeeda25ea2ffbd8e6b96e038356fb
marilia-borgo <marilia.bm01@gmail.com>
2022-06-07 14:02:40 -0300

    ex 16

--- secao_03_estrutura_de_repeticao/ex_16_fibonnacci_ate_valor_maior_que_500.py
+++ secao_03_estrutura_de_repeticao/ex_16_fibonnacci_ate_valor_maior_que_500.py
@@ -12,3 +12,16 @@ A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... 
 
 def calcular_serie_de_fibonacci_ate_valor_ser_maior_que_500() -> str:
     """Escreva aqui em baixo a sua solução"""
+    t1 = 1
+    t2 = 0
+    termo = 0
+    while not termo > 500:
+        if termo == 0:
+            print(f"'{termo}", end=", ")
+        else:
+            print(f"{termo}", end=", ")
+        termo = t1+t2
+        t1 = t2
+        t2 = termo
+        if termo >= 500:
+            print(f"{termo}'")
