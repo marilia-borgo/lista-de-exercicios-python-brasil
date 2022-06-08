0dc5b2a657ddc1c8423129def2bada33ec07bc71
marilia-borgo <marilia.bm01@gmail.com>
2022-06-08 13:18:37 -0300

    ex 17 a 19

--- secao_03_estrutura_de_repeticao/ex_18_estatisticas_de_n_numeros.py
+++ secao_03_estrutura_de_repeticao/ex_18_estatisticas_de_n_numeros.py
@@ -18,3 +18,12 @@ Faça um programa que, dado um conjunto de N números, determine o menor valor, 
 
 def calcular_estatisticas(*numeros) -> str:
     """Escreva aqui em baixo a sua solução"""
+    conta = [*numeros]
+    if conta == []:
+        return 'Maior valor: não existe. Menor valor: não existe. Soma: 0'
+
+    else:
+        conta.sort()
+        return f'Maior valor: {conta[-1]}. Menor valor: {conta[0]}. Soma: {sum(conta)}'
+
+    
